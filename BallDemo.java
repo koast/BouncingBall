a3c86334028988c4842b1d3bcc3904326bc540aa
Alejandro Aller <Alejandroallerdiez@gmail.com>
2014-04-04 12:14:28 +0200

    Permite seleccionar el numero de pelotas que deseas que boten

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.util.Random;
+import java.util.ArrayList;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -11,42 +13,48 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
-
+    private ArrayList<BouncingBall> balls;
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
+        balls = new ArrayList<>();
         myCanvas = new Canvas("Ball Demo", 600, 500);
     }
 
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numberOfBalls)
     {
         int ground = 400;   // position of the ground line
-
         myCanvas.setVisible(true);
-
+        for(int i = 0 ; i < numberOfBalls ; i++)
+        {
+            balls.add(new BouncingBall(50,50,16,Color.BLUE, ground,myCanvas));
+        }
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
-
+        for(BouncingBall showBalls: balls)
+        {
+            showBalls.draw();
+        }
         // make them bounce
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
+            for(BouncingBall showBalls: balls)
+            {
+                showBalls.move();
+                   if(showBalls.getXPosition() >= 550)
+         {
                 finished = true;
+            }
+            // stop once ball has travelled a certain distance on x axis
+         
             }
         }
     }
